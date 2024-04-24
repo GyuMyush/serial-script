009fc6bf66dbd28e5294ee86ce19ece208a77d9d
artem <33878928+GyuMyush@users.noreply.github.com>
2024-04-24 14:24:09 +0300

    Update serialDingoE200.sh
    Исправление ошибок вызова и коментарии

--- serialDingoE200.sh
+++ serialDingoE200.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 if [ `whoami` != root ]; then
-	echo 'Error: The script should be run with root privileges' ;
+	echo 'Error: Скрипт должен быть запущен с правами суперпользователя' ;
 	exit
 fi
 
-echo 'Waiting for thermometer to initialize...'
+echo 'Ожидание инициализации термометра...'
 
-# Send the command to the thermometer to initialize
 $(sleep 0.1 && echo '$i0000#013#010' > /dev/ttyACM0) &
 
-# Wait for the thermometer to initialize
+
 responce=$(head -n 40 /dev/ttyACM0)
 wait $!
 
-echo 'Thermometer initialized:'
+echo 'Алкотестер инициализирован:'
 echo $responce
 wait $!
 
 # Print thermometer information
-echo 'Thermometer information:';
+echo 'Информация о термометре:';
 lsusb -d 10c4:ea60 -v | grep iSerial
 
-echo 'Running alcohol tester script...'
-
-# Run the alcohol tester script
-# Replace "alcohol_tester.sh" with the actual path to the alcohol tester script
-./alcohol_tester.sh
-
-echo 'Done.'
+echo 'Готово.'
